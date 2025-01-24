630709f3c8063cfc6fa7424fdead65fb9097669b
gaik180303 <149417472+gaik180303@users.noreply.github.com>
2025-01-25 00:03:29 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -3,7 +3,8 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 //console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
-    // Handle connection
+// Handle connection\
+connection.write(`+PONG\r\n`);
   });
 // Uncomment this block to pass the first stage
 // const server = net.createServer((connection) => {
@@ -11,3 +12,8 @@ const server = net.createServer((connection) => {
 // });
 //
  server.listen(6379, "127.0.0.1");
+
+
+//  git add .
+// git commit --allow-empty -m "[any message]"
+// git push origin master
