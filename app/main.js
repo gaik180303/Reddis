16e53bbba9bd7e96aab33873fe2d059ff4a01b65
gaik180303 <149417472+gaik180303@users.noreply.github.com>
2025-01-25 00:45:06 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -4,13 +4,25 @@ const net = require("net");
 //console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
 // Handle connection\
-connection.write(`+PONG\r\n`);
-  });
+// connection.write(`+PONG\r\n`);
+//   });
 // Uncomment this block to pass the first stage
-// const server = net.createServer((connection) => {
-//   // Handle connection
-// });
-//
+const server = net.createServer((connection) => {
+//    Handle connection
+connection.on('data',(data)=>{
+    const commands =data.toString().split('\r\n');  //Clients send data in the RESP (Redis Serialization Protocol) format, where commands are delimited by \r\n.
+    //Splits the string into an array of commands, where each command is separated by \r\n (carriage return and newline).
+    
+    for(const command of commands)
+    {
+        if(command.toUpperCase()==='PING')
+        {
+            connection.write('+PONG\r\n');
+        }
+    }
+})
+});
+
  server.listen(6379, "127.0.0.1");
 
 
