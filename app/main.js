727fa616948699c470bab28c628553d355d94933
gaik180303 <149417472+gaik180303@users.noreply.github.com>
2025-01-25 17:32:05 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-const net = require("net");
+const net = require("net"); // for creating the tcp server
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 //console.log("Logs from your program will appear here!");
@@ -7,9 +7,9 @@ const net = require("net");
 // connection.write(`+PONG\r\n`);
 //   });
 // Uncomment this block to pass the first stage
-const server = net.createServer((connection) => {
+const server = net.createServer((connection) => { //  new tcp server
 //    Handle connection
-connection.on('data',(data)=>{
+connection.on('data',(data)=>{ // handeling incoming data
     const commands =data.toString().split('\r\n');  //Clients send data in the RESP (Redis Serialization Protocol) format, where commands are delimited by \r\n.
     //Splits the string into an array of commands, where each command is separated by \r\n (carriage return and newline).
     
@@ -20,7 +20,10 @@ connection.on('data',(data)=>{
             connection.write('+PONG\r\n');
         }
     }
+   
+
 })
+connection.on("end",()=>console.log("Client disconnected")); // connection is  asocket class which represent the connection between the server and client
 });
 
  server.listen(6379, "127.0.0.1");
